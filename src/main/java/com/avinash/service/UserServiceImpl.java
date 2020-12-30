c52bb8d541c983ecb2b5570827364a183464ba54
amanick24 <mravinash002@gmail.com>
2020-12-31 02:14:51 +0530

    Implemented java mail sender using template

--- src/main/java/com/avinash/service/UserServiceImpl.java
+++ src/main/java/com/avinash/service/UserServiceImpl.java
@@ -1,11 +1,20 @@
 package com.avinash.service;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.avinash.dao.CityRepository;
@@ -16,6 +25,7 @@ import com.avinash.entity.CityEntity;
 import com.avinash.entity.CountryEntity;
 import com.avinash.entity.StateEntity;
 import com.avinash.entity.UserEntity;
+
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -28,6 +38,8 @@ public class UserServiceImpl implements UserService {
 	private CityRepository cityRepo;
 	@Autowired
 	private UserRepository userRepo;
+	@Autowired
+	private JavaMailSender mailSender;
 
 	@Override
 	public boolean saveUser(UserEntity userEntity) {
@@ -35,9 +47,15 @@ public class UserServiceImpl implements UserService {
 		userEntity.setPassword(passwordGenerator(8));
 		userEntity.setAccStatus("LOCKED");
 
+		String filename = "UNLOCK-ACC-EMAIL-BODY-TEMPLATE.TXT";
 		UserEntity saveObj = userRepo.save(userEntity);
+		String emailBody = getEmailBody(userEntity, filename);
+		String subject = "Unlock your account";
+		
+		boolean issentMail = sendMail(subject, emailBody, userEntity.getEmailId());
 
-		return saveObj.getUserId() != null;
+		//return saveObj.getUserId() != null;
+		return saveObj.getUserId() != null && issentMail;
 	}
 
 	@Override
@@ -96,20 +114,21 @@ public class UserServiceImpl implements UserService {
 	public String loginCheck(String emailId, String pwd) {
 		UserEntity userObj = userRepo.findByEmailIdAndPassword(emailId, pwd);
 
-		if (userObj.getPassword() == pwd) {
+		if (userObj.getPassword().equals(pwd)) {
 			if (userObj.getAccStatus() == "LOCKED") {
 				return "Please UNLOCK your passoword";
 			}
-			return "Valid password";
+			return "Valid";
 		}
 
-		return "Username/Password Invalid";
+		return "Invalid";
 	}
 
 	@Override
 	public boolean isTempPwdValid(String emailId, String tempPwd) {
 
 		UserEntity userObj = userRepo.findByEmailId(emailId);
+		if(userObj == null) { return false;}
 		String password = userObj.getPassword();
 		if (password.equals(tempPwd)) {
 			return true;
@@ -135,10 +154,21 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public String forgotPassword(String emailId) {
 		UserEntity userObj = userRepo.findByEmailId(emailId);
-		if (userObj.getEmailId() != null) {
+		if (userObj != null) {
 			// TODO Logic to send Temp password to email
-			// TODO Update the password to temppwd
-			return "Mail sent successfully ";
+			userObj.setPassword(passwordGenerator(8));
+			userObj.setAccStatus("LOCKED");
+			userRepo.save(userObj);
+			String filename= "FORGOT-PASSWORD-EMAIL-BODY-TEMPLATE.TXT";
+			String emailBody = getEmailBody(userObj, filename);
+			String subject = "Reset password";
+			
+			boolean issentMail = sendMail(subject, emailBody, userObj.getEmailId());
+			if(issentMail == true) {
+				return "Mail sent successfully ";	
+			}
+			
+			return "Could not send mail";
 		}
 
 		return "Invalid Email address";
@@ -160,5 +190,65 @@ public class UserServiceImpl implements UserService {
 			}
 		return new String(pass);
 	}
+	
+	public boolean sendMail(String subject, String body, String to) {
+		boolean isSent = false;
+		try {
+			//SimpleMailMessage msg = new SimpleMailMessage();
+			MimeMessage mailMessage = mailSender.createMimeMessage();
+			MimeMessageHelper mime = new MimeMessageHelper(mailMessage);
+
+			mime.setSubject(subject);
+			mime.setTo(to);
+			mime.setText(body, true);
+			/*
+			 * msg.setSubject(subject); msg.setTo(to); msg.setText(body);
+			 */
+			
+			mailSender.send(mailMessage);
+			isSent = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return isSent;
+	}
+	
+	private String getEmailBody(UserEntity userEntity, String fileName) {
+		/*
+		 * StringBuffer sb = new StringBuffer(""); try { File f = new File(filename);
+		 * FileReader fr= new FileReader(f); BufferedReader br = new BufferedReader(fr);
+		 * String line = br.readLine(); while(line != null) {
+		 * if(line.contains("{FNAME}")){ line =
+		 * line.replace("{FNAME}",userEntity.getfName()); } if(line.contains("{LNAME}"))
+		 * { line = line.replace("{LNAME}",userEntity.getlName()); }
+		 * 
+		 * if(line.contains("{TEMP-PWD}")) { line = line.replace("{TEMP-PWD}",
+		 * userEntity.getPassword()); } if(line.contains("{EMAIL}")) {
+		 * 
+		 * line = line.replace("{EMAIL}", userEntity.getEmailId()); }
+		 * 
+		 * sb.append(line);
+		 * 
+		 * line = br.readLine(); } br.close();}
+		 */	 
+	List<String> replacedLines = null;
+	String mailBody = null;
+	try {
+		Path path = Paths.get(fileName, "");
+		Stream<String> lines = Files.lines(path);
+		replacedLines = lines.map(line -> line.replace("{FNAME}", userEntity.getfName())
+									.replace("{LNAME}", userEntity.getlName())
+									.replace("{TEMP-PWD}", userEntity.getPassword())
+									.replace("{EMAIL}", userEntity.getEmailId()))
+									.collect(Collectors.toList());
+		mailBody = String.join("", replacedLines);
+	}
+	
+	catch (Exception e) {
+		e.printStackTrace();
+	}
+
+	return mailBody;
+}
 
 }
